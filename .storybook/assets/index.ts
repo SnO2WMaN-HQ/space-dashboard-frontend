8261b0a466769d810560232991ee3ec9493bd627
SnO2WMaN <me@sno2wman.net>
2021-05-14 04:05:49 +0900

    chore: Storybookの諸々

--- .storybook/assets/index.ts
+++ .storybook/assets/index.ts
@@ -4,6 +4,8 @@ const icons = [
   require('./icons/3.png'),
   require('./icons/4.png'),
   require('./icons/5.png'),
+  require('./icons/6.png'),
+  require('./icons/7.png'),
 ];
 
 export const random = {
