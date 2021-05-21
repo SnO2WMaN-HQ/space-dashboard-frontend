c96d013d118badf5a5021849aee23fbf9e579efb
SnO₂WMaN <sno2wman.official@gmail.com>
2021-05-21 13:07:54 +0900

    feat: fallbackをblockingに変更 (#16)

--- src/pages/users/[unique]/index.tsx
+++ src/pages/users/[unique]/index.tsx
@@ -27,7 +27,7 @@ export const getStaticPaths: GetStaticPaths<UrlQuery> = async () => {
     )
     .then((paths) => ({
       paths,
-      fallback: true,
+      fallback: 'blocking',
     }));
 };
 
