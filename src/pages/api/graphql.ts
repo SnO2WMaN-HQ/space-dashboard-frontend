d4673795d94e44f1758652030b3e7e0235a6c99e
SnO2WMaN <me@sno2wman.net>
2021-05-21 12:19:03 +0900

    fix: graphqlエンドポイント周り修正

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -6,18 +6,23 @@ import {NextApiHandler} from 'next';
 const client = new GraphQLClient(process.env.API_GRAPHQL_ENDPOINT!);
 
 const handler: NextApiHandler = async (req, res) => {
-  const {accessToken} = await getAccessToken(req, res, {});
-
-  const {data, status, errors} = await client.rawRequest(
-    req.body.query,
-    req.body.variables,
-    {Authorization: `Bearer ${accessToken}`},
-  );
-
-  if (status === 200) {
-    res.json({data});
-  } else {
-    res.status(status).json({code: status, errors});
+  try {
+    const header = await getAccessToken(req, res, {})
+      .then(({accessToken}) => ({
+        Authorization: `Bearer ${accessToken}`,
+      }))
+      .catch(() => ({}));
+    const data = await client.request(
+      req.body.query,
+      req.body.variables,
+      header,
+    );
+    res.send({data});
+  } catch (error) {
+    res.status(error.status || 500).json({
+      code: error.code,
+      error: error.message,
+    });
   }
 };
 export default withApiAuthRequired(handler);
