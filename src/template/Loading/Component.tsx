99fd192fb363e21c6babaf1487076ef6e3a23f27
SnO2WMaN <me@sno2wman.net>
2021-05-26 10:35:15 +0900

    feat: Loading

--- src/template/Loading/Component.tsx
+++ src/template/Loading/Component.tsx
@@ -2,6 +2,7 @@ import {useTranslation} from 'next-i18next';
 import React from 'react';
 import {tw} from 'twind';
 import {IconLoading} from '~/components/atoms/Icon';
+import {PageLayout} from '~/components/layouts/PageLayout';
 
 export type ComponentProps = {
   className?: string;
@@ -9,28 +10,25 @@ export type ComponentProps = {
 export const Component: React.FC<ComponentProps> = ({children, className}) => {
   const {t} = useTranslation();
   return (
-    <main
-      className={tw(
-        className,
-        'bg-gray-200',
-        'flex',
-        'flex-col',
-        'justify-center',
-        'items-center',
-      )}
+    <PageLayout
+      className={tw(className, ['flex', 'justify-center', 'items-center'])}
     >
-      <IconLoading className={tw('text-blue-500', 'text-6xl')} />
-      <span
-        className={tw(
-          'mt-2',
-          'text-sm',
-          'tracking-wider',
-          'text-gray-600',
-          'font-bold',
-        )}
+      <div
+        className={tw(['flex', 'flex-col', 'justify-center', 'items-center'])}
       >
-        {t('loading')}
-      </span>
-    </main>
+        <IconLoading className={tw('text-blue-500', 'text-6xl')} />
+        <span
+          className={tw(
+            'mt-2',
+            'text-sm',
+            'tracking-wider',
+            'text-gray-600',
+            'font-bold',
+          )}
+        >
+          {t('loading')}
+        </span>
+      </div>
+    </PageLayout>
   );
 };
